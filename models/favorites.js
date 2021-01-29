6a2b713c1ae40d0c1bcfe949e0bdc1243c65ac68
Anzel Capparelli <anzelcapparelli@gmail.com>
2021-01-29 09:20:50 -0800

    created db (and provided schema for teammates to duplicate); changed config.json from starter code to target db; created rough favorites model; set up many-to-many association code (junction table)

---
+++ models/favorites.js
@@ -0,0 +1,49 @@
+// Creating our User model
+module.exports = function (sequelize, DataTypes) {
+    const Favorite = sequelize.define("Favorite", {
+
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+
+            validate: {
+                len: [1]
+            }
+        },
+
+        location: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+
+        phone: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+
+        },
+
+        latitude: {
+            type: DataTypes.INTEGER,
+        },
+
+        latitude: {
+            type: DataTypes.INTEGER,
+        }
+
+
+    });
+
+    // ============================================================================================================================================================
+    // code that hanndles many-to-many association
+
+    // adding assoctiation with other model, allowing many-to-many, through a junction table
+    Favorite.associate = function (models) {
+        // Associating Author with Posts
+        // When an Author is deleted, also delete any associated Posts
+        Favorite.belongsToMany(models.User, {
+            through: "user_favorite",
+            as: "user",
+            foreignKey: "user_id"
+        });
+    };
+};  
---
+++ schema.sql
@@ -0,0 +1,4 @@
+-- Drops the database if it exists currently --
+DROP DATABASE IF EXISTS brew_budz;
+-- Creates the "brew_budz" database --
+CREATE DATABASE brew_budz;
